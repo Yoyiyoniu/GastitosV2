d965b1c8cb979eafc0432a50812a035495d34a38
Yoyoz <yoyiyomeco@hotmail.com>
2025-10-31 15:36:50 -0700

    feat: init android and add app icon

---
+++ src-tauri/gen/android/app/src/main/java/com/yoyoz/gastitos/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.yoyoz.gastitos
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
